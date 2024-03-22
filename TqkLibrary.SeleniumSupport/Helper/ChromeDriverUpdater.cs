16c5b967c5f20932f541b68e1900f4d1c5cc9baf
tqk2811 <tqk2811@gmail.com>
2024-03-22 19:22:05 +0700

    rework ChromeDriverUpdater

--- TqkLibrary.SeleniumSupport/Helper/ChromeDriverUpdater.cs
+++ TqkLibrary.SeleniumSupport/Helper/ChromeDriverUpdater.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.IO.Pipes;
 using System.Linq;
 using System.Net.Http;
 using System.Threading;
@@ -22,21 +23,21 @@ namespace TqkLibrary.SeleniumSupport
         /// 
         /// </summary>
         /// <param name="folderLocation"></param>
-        /// <param name="MajorPart"></param>
         /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        public static Task<int> DownloadAsync(string folderLocation, int MajorPart, CancellationToken cancellationToken = default)
+        /// <returns>Directory contain chromedriver.exe</returns>
+        public static Task<string> DownloadAsync(string folderLocation, CancellationToken cancellationToken = default)
         {
-            return DownloadAsync(folderLocation, MajorPart, GetChromePath(), cancellationToken);
+            return DownloadAsync(folderLocation, GetChromePath(), cancellationToken);
         }
         /// <summary>
         /// 
         /// </summary>
-        /// <returns></returns>
-        public static async Task<int> DownloadAsync(string folderLocation, int MajorPart, string chromePath, CancellationToken cancellationToken = default)
+        /// <returns>Directory contain chromedriver.exe</returns>
+        public static async Task<string> DownloadAsync(string folderLocation, string chromePath, CancellationToken cancellationToken = default)
         {
             var version = GetChromeVersion(chromePath);
-            if (version.FileMajorPart > MajorPart || !IsChromeDriverExist(folderLocation))
+            string targetDir = Path.Combine(folderLocation, version.FileVersion);
+            if (!File.Exists(Path.Combine(targetDir, "chromedriver.exe")))
             {
                 using HttpClient httpClient = new HttpClient(new HttpClientHandler()
                 {
@@ -54,16 +55,15 @@ namespace TqkLibrary.SeleniumSupport
                 {
                     urlToDownload = await httpClient.GetURLToDownloadAsync(verString, cancellationToken).ConfigureAwait(false);
                 }
-                KillAllChromeDriverProcesses();
-                await httpClient.DownloadNewVersionOfChromeAsync(folderLocation, urlToDownload, cancellationToken);
+                return await httpClient.DownloadNewVersionOfChromeAsync(targetDir, urlToDownload, cancellationToken);
             }
-            return version.FileMajorPart;
+            else
+            {
+                return targetDir;
+            }
         }
 
-        static bool IsChromeDriverExist(string folderLocation)
-        {
-            return File.Exists(Path.Combine(folderLocation, "chromedriver.exe"));
-        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -202,24 +202,7 @@ namespace TqkLibrary.SeleniumSupport
             return "https://chromedriver.storage.googleapis.com/" + html + "/chromedriver_win32.zip";
         }
 
-        static void KillAllChromeDriverProcesses()
-        {
-            //It's important to kill all processes before attempting to replace the chrome driver, because if you do not you may still have file locks left over
-            var processes = Process.GetProcessesByName("chromedriver");
-            foreach (var process in processes)
-            {
-                try
-                {
-                    process.Kill();
-                }
-                catch
-                {
-                    //We do our best here but if another user account is running the chrome driver we may not be able to kill it unless we run from a elevated user account + various other reasons we don't care about
-                }
-            }
-        }
-
-        static async Task DownloadNewVersionOfChromeAsync(this HttpClient httpClient, string folderLocation, string urlToDownload, CancellationToken cancellationToken = default)
+        static async Task<string> DownloadNewVersionOfChromeAsync(this HttpClient httpClient, string tarGetDir, string urlToDownload, CancellationToken cancellationToken = default)
         {
             if (string.IsNullOrEmpty(urlToDownload)) throw new ArgumentException("Unable to get url because urlToDownload is empty");
 
@@ -228,33 +211,25 @@ namespace TqkLibrary.SeleniumSupport
             using HttpResponseMessage httpResponseMessage = await httpClient.SendAsync(httpRequestMessage, HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
             httpResponseMessage.EnsureSuccessStatusCode();
 
-            if (Directory.Exists(folderLocation))
+            Directory.CreateDirectory(tarGetDir);
+
+
+            using MemoryStream memoryStream = new MemoryStream();
+
+            using Stream stream = await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
+            await stream.CopyToAsync(memoryStream, 81920, cancellationToken).ConfigureAwait(false);
+
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            using ZipArchive zipArchive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+            ZipArchiveEntry? entry = zipArchive.Entries.FirstOrDefault(x => x.Name.Contains("chromedriver.exe"));
+            if (entry is not null)
             {
-                Directory.Delete(folderLocation, true);
+                entry.ExtractToFile(Path.Combine(tarGetDir, "chromedriver.exe"), true);
+                return tarGetDir;
             }
-
-            Directory.CreateDirectory(folderLocation);
-
-            string zipPath = Path.Combine(folderLocation, "chromedriver.zip");
-
-            using (FileStream fileStream = new FileStream(zipPath, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+            else
             {
-                using Stream stream = await httpResponseMessage.Content.ReadAsStreamAsync().ConfigureAwait(false);
-                await stream.CopyToAsync(fileStream, 81920, cancellationToken).ConfigureAwait(false);
-            }
-
-            if (File.Exists(zipPath))
-            {
-                using var zip = ZipFile.Open(zipPath, ZipArchiveMode.Read);
-                var entry = zip.Entries.FirstOrDefault(x => x.Name.Contains("chromedriver.exe"));
-                if (entry is not null)
-                {
-                    entry.ExtractToFile(Path.Combine(folderLocation, "chromedriver.exe"), true);
-                }
-                else
-                {
-                    throw new Exception($"chromedriver.exe not found in zip file");
-                }
+                throw new Exception($"chromedriver.exe not found in zip file");
             }
         }
     }
